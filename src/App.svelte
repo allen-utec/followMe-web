c64d117b4615292ec5a51e5ffefd80eb867000fa
Allen Joseph <allen.velasco@utec.edu.pe>
2023-06-26 18:48:31 -0500

    feat: watch location changes

--- src/App.svelte
+++ src/App.svelte
@@ -3,27 +3,43 @@
   import "leaflet/dist/leaflet.css";
 
   let consoleOpened = false;
+  let map: L.Map;
+  let routeCoords = [];
 
   document.addEventListener("DOMContentLoaded", () => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
+    map = L.map("map").fitWorld();
 
-      const map = L.map(document.getElementById("map"), {
-        center: [lat, lon],
-        zoom: 16,
-      });
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
 
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }).addTo(map);
-    });
+    map.locate({ setView: true, maxZoom: 16, watch: true });
+
+    map.on("locationfound", onLocationFound);
   });
 
   function toggleConsole() {
     consoleOpened = !consoleOpened;
+  }
+
+  function onLocationFound(e: L.LocationEvent) {
+    routeCoords = routeCoords.concat(e.latlng);
+
+    if (routeCoords.length === 1) {
+      L.polyline(routeCoords, { color: "blue" }).addTo(map);
+    } else {
+      map.eachLayer((layer) => {
+        if (layer instanceof L.Polyline) {
+          layer.setLatLngs(routeCoords);
+        } else if (layer instanceof L.Marker) {
+          layer.remove();
+        }
+      });
+    }
+
+    L.marker(e.latlng).addTo(map).bindPopup("Aquí tas tú!").openPopup();
   }
 </script>
 
@@ -36,12 +52,23 @@
     </button>
 
     <div id="map" />
-    <a href="#!" on:click={toggleConsole}>
+
+    <a class="console-link" href="#!" on:click={toggleConsole}>
       {consoleOpened ? "hide" : "show"} console</a
     >
   </div>
 
-  <div id="console" class:openend={consoleOpened} />
+  <div id="console" class:openend={consoleOpened}>
+    <ul>
+      {#each routeCoords as coord}
+        <li>
+          <span>Lat: {Number(coord.lat).toFixed(6)}</span><span
+            >Lng: {Number(coord.lng).toFixed(6)}</span
+          >
+        </li>
+      {/each}
+    </ul>
+  </div>
 </main>
 
 <style>
@@ -54,11 +81,13 @@
 
   #console {
     position: absolute;
-    width: 640px;
+    width: 500px;
     height: 100%;
     top: 0;
     background: #000;
-    right: -640px;
+    right: -500px;
+    padding: 0;
+    z-index: 9999;
   }
 
   #console.openend {
@@ -72,14 +101,49 @@
   }
 
   #content.openend {
-    margin-right: 640px;
+    margin-right: 500px;
     animation-name: shrink;
     animation-duration: 0.2s;
   }
 
+  #console > ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  #console > ul > li {
+    color: #ddd;
+    padding: 2px 8px;
+    border-bottom: 1px solid #999;
+    display: flex;
+    gap: 8px;
+  }
+
+  .console-link {
+    display: block;
+    padding: 8px;
+  }
+
+  @media screen and (max-width: 640px) {
+    #map {
+      height: 500px;
+    }
+
+    #content.openend {
+      margin-right: 0;
+      animation: none;
+    }
+
+    #console {
+      width: 100%;
+      height: 50%;
+    }
+  }
+
   @keyframes slidein {
     from {
-      right: -640px;
+      right: -500px;
     }
     to {
       right: 0;
@@ -91,7 +155,7 @@
       margin-right: 0;
     }
     to {
-      margin-right: 640px;
+      margin-right: 500px;
     }
   }
 </style>
